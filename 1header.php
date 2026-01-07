c3f31bff0d45ac90c7a4721008bbe74ca500a2a4
Kiwil <13.kristycuculg@gmail.com>
2026-01-07 13:25:57 -0600

    Update 1header.php

--- 1header.php
+++ 1header.php
@@ -172,7 +172,7 @@
       </li>
 
       <li class="nav-item mb-3">
-        <a class="nav-link d-flex align-items-center" href="1Ubicación.php">
+        <a class="nav-link d-flex align-items-center" href="1ubicacion.php">
           <i class="bi bi-geo-alt-fill me-2"></i>Location
         </a>
       </li>
@@ -238,4 +238,5 @@
 
 </body>
 </html>
+
 
